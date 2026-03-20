c50aab59ae6b14bae8074dd1826234ce1e69ba9e
Nathan Livesay <nmlivesa@gmail.com>
2026-03-19 20:18:01 -0500

    Built template player state machine

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,15 @@
+class_name Player
 extends CharacterBody2D
 
 @onready
 var animations = $animations
 @onready 
 var state_machine = $state_machine
+@onready
+var move_component = $move_component
 
 func _ready() -> void:
-	state_machine.init(self)
+	state_machine.init(self, animations, move_component)
 
 func _unhandled_input(event: InputEvent) -> void:
 	state_machine.process_input(event)
@@ -16,26 +19,3 @@ func _physics_process(delta: float) -> void:
 
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
-
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-#
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
